6d298c8e7cffce3dd814718f6d822d0923497d26
Will Allen <wallen2268@gmail.com>
2020-08-13 02:15:25 -0500

    Fixed navigation from core->chatrooms->chat

--- Components/ChatRoomsPage/ChatRoomScreenHeader.js
+++ Components/ChatRoomsPage/ChatRoomScreenHeader.js
@@ -4,21 +4,21 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import LeftActionIcon from "../../assets/svg/./LeftActionIcon.js"
 import { useNavigation } from '@react-navigation/native';
 
-export class ChatScreenHeader extends Component{
+class ChatRoomsScreenHeader extends Component{
 	
 
 	render(){
 		const { navigation } = this.props;
 		return(
 			<View style={{flex: 1, flexDirection: 'row'}}>
-			<TouchableOpacity onPress={() => this.props.navigation.popToTop()}>
+			<TouchableOpacity onPress={() => navigation.pop()}>
 				<View style={{flex:1, justifyContent: 'center', width:100}}>
-						<LeftActionIcon style={{alignSelf: 'flex-start'}}/>
+					<LeftActionIcon style={{alignSelf: 'flex-start'}}/>
 				</View>
 			</TouchableOpacity>
 				
 				<View style={{flex:1, justifyContent: 'center'}}>
-					<Text style={{fontSize: 18, fontWeight: 'bold', alignSelf: 'center', marginTop: '10%'}}>Everett</Text>
+					<Text style={{fontSize: 18, fontWeight: 'bold', alignSelf: 'center', marginTop: '10%'}}>Chats</Text>
 				</View>
 
 				<View style={{flex:1}}>
@@ -31,5 +31,5 @@ export class ChatScreenHeader extends Component{
 
 export default function(props) {
   const navigation = useNavigation();
-  return <ChatScreenHeader {...props} navigation={navigation} />;
+  return <ChatRoomsScreenHeader {...props} navigation={navigation} />;
 }
--- Components/ChatRoomsPage/ChatRoomsScreen.js
+++ Components/ChatRoomsPage/ChatRoomsScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
-import ChatScreenHeader  from "./ChatRoomScreenHeader"
+import ChatRoomsScreenHeader  from "./ChatRoomScreenHeader"
 import ChatRoomList from "./ChatRoomList"
 
 export default class ChatRoomsScreen extends Component {
@@ -15,7 +15,7 @@ export default class ChatRoomsScreen extends Component {
 		return(
 	      <View style={{flex:1}}>
 	        <View style={{flex:2, backgroundColor: "#FFFFFF"}}>
-	          <ChatScreenHeader/>
+	          <ChatRoomsScreenHeader navigation={this.props.navigation}/>
 	        </View>
 
 	        <View style={{flex:14, backgroundColor: "#FFFFFF", borderColor: '#D5D5D5', borderWidth: 1.5}}>
