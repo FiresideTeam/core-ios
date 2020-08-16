3ec4d6c4ad411ece1b76b714f8d8533e5086eef5
Will Allen <wallen2268@gmail.com>
2020-08-16 18:59:19 -0500

    Socket restructuring

--- redux-saga/watchers/SocketWatchers.js
+++ redux-saga/watchers/SocketWatchers.js
@@ -1,12 +1,22 @@
 import { combineReducers } from "redux";
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, takeLatest, take, fork, call, spawn } from "redux-saga/effects";
 
-import { watchSocketChannel } from "../sagas/SocketSagas";
+import { openSocket, readSocket, writeSocket } from "../sagas/SocketSagas";
 
 import * as actions from "../../redux/constants/ActionTypes";
 
 export function* watchSocket() {
-  yield takeEvery(actions.CREATE_SOCKET, watchSocketChannel);
-  //yield takeEvery(actions.SOCKET_SEND, writeSocket);
-  //yield takeEvery(actions.SOCKET_RECIEVE, )
+  // Don't do anything until a CREATE_SOCKET event is retrieved
+  yield take(actions.CREATE_SOCKET);
+  yield fork(openSocket)
+  
+  // Wait for SOCKET_CREATED action to spawn read & write sagas
+  const { socket } = yield take(actions.SOCKET_CREATED);
+  
+  // Spawn two sagas for read & write
+  // Maybe change this to fork, I need to read more
+  yield spawn(readSocket, socket)
+  yield spawn(writeSocket, socket); 
 }
+
+
