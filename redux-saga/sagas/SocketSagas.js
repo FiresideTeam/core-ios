97a33be0dfa9621a1b01afc3468889ba335dd58e
Will Allen <wallen2268@gmail.com>
2020-08-17 00:47:43 -0500

    Socket housecleaning success and a stable build

--- redux-saga/sagas/SocketSagas.js
+++ redux-saga/sagas/SocketSagas.js
@@ -29,15 +29,16 @@ function* openSocket() {
 // Will need to change later with rooms & content fetch
 function createSocketChannel(socket, channels, auth) {
   return eventChannel((emit) => {
-    const eventHandler = (event) => {
-      emit(event);
+    const messagesCreatedEvent = (event) => {
+      console.log('event', event)
+      emit({type: actions.RECEIVED_MESSAGES, ...event});
     };
 
     const errorHandler = (errorEvent) => {
       emit(new Error(errorEvent.reason));
     };
 
-    const connectHandler = (socket) => {
+    const authHandler = () => {
       console.log("Fireside [Socket] : connected to the server");
       // Pass auth in as a param later, to async storage stuff outside of this callback
       if (false) {
@@ -74,11 +75,11 @@ function createSocketChannel(socket, channels, auth) {
     const roomConnectHandler = () => {};
 
     // On connect, attempt to authenticate
-    socket.on("connect", (socket) => connectHandler);
+    socket.on("connect", authHandler);
 
     // Replace this with a for loop
     // loop thru all the rooms and subscribe to their events?
-    socket.on("messages created", eventHandler);
+    socket.on("messages created", messagesCreatedEvent);
 
     socket.on("error", errorHandler);
 
@@ -120,23 +121,22 @@ function* authenticate() {
 
 // Handles and routes events emitted from createSocketChannel
 function* readSocket(socket) {
-  console.log("read socket", socket);
   const socketChannel = yield call(createSocketChannel, socket);
-  console.log("read socket");
   while (true) {
     try {
       const payload = yield take(socketChannel);
-
       switch (payload.type) {
-        case payload.type == actions.RECEIVED_MESSAGES:
+        case actions.RECEIVED_MESSAGES:
+          console.log('dada')
           yield put({
             type: actions.RECEIVED_MESSAGES,
             id: payload.messageId,
             text: payload.text,
             user: { name: payload.name },
           });
-        case payload.type == actions.SOCKET_CREATED:
+        case actions.SOCKET_CREATED:
           yield put({ type: actions.SOCKET_CREATED });
+
       }
     } catch (err) {
       console.log("socket error: ", err);
